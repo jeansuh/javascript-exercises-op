146cc98b788e3b7055b3c18380762c5d1d66ac16
JeanSuh <scenario.529@gmail.com>
2023-12-19 15:33:50 -0500

    exercise 2 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(originalString, repeatNumber) {
+	if(repeatNumber < 0) return "ERROR"
+	let newString = ""
+	let iterator = 0
+	for(let i = 0; i < repeatNumber; i++){
+		newString += originalString
+	}
+	return newString
 };
 
 // Do not edit below this line
